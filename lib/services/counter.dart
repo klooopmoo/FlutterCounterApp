b507392f3df83ee8da440f8ca23da8f891a6493e
klooopmoo <gc.franco93@gmail.com>
2022-07-07 10:45:01 -0700

    changed ngrok link to localhost:8081

--- lib/services/counter.dart
+++ lib/services/counter.dart
@@ -4,15 +4,13 @@ import 'package:http/http.dart' as http;
 
 class Counter{
   int count;//counters to keep track of state
-  final String baseUrl = "https://094d-2603-8000-1d00-2b85-b4ca-de3d-16f4-a8b1.ngrok.io";
+  final String baseUrl = "localhost:8081";
   Counter({this.count});
   Future<int> getCount()async {
     var url = Uri.parse("$baseUrl/state");
     http.Response response = await http.get(url);
     Map data = jsonDecode(response.body);
     count = data['counter'];
-    print(data);
-    print(count);
     return count;
 
   }
